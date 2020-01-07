fd9fe658e04eb899c76088bb01d90a4dacdce239
xa-xa-xa <askrasn@gmail.com>
2020-01-07 14:28:13 -0800

    misc fixes

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -7,9 +8,8 @@ const CreateExercise = () => {
   const initialState = {
     username: '',
     description: '',
-    duration: 0,
-    date: new Date(),
-    users: []
+    duration: '',
+    date: new Date()
   };
 
   const activity = [
@@ -23,9 +23,10 @@ const CreateExercise = () => {
     'tennis'
   ];
 
-  // const [user, setUser] = useState('test user');
-
   const [exercise, setExercise] = useState(initialState);
+
+  const [users, setUsers] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   const onDateChange = e => setExercise({ ...exercise, date: e });
 
@@ -37,35 +38,59 @@ const CreateExercise = () => {
     }
   };
 
-  // useEffect(() => {
-
-  //   return () => {};
-  // }, []);
-
   const onSubmit = e => {
     e.preventDefault();
-    // window.location = '/';
+
+    axios
+      .post('http://localhost:5000/exercises/add', exercise)
+      .then(res => {
+        if (res.status === 200) window.location = '/';
+      })
+      .catch(err => {
+        console.log('ADD Exercise ERROR(fe): ', err);
+      });
   };
 
-  const { duration, date, description, username, users } = exercise;
+  useEffect(() => {
+    axios.get('http://localhost:5000/users/').then(res => {
+      if (res.data.length > 0) {
+        setUsers(res.data.map(user => user.username));
+        setLoaded(true);
+      }
+    });
+
+    return () => {};
+  }, []);
+
+  const { duration, date, description, username } = exercise;
 
   return (
-    <div className='flex justify-center mt-8'>
-      <form className='md:w-3/4 m-0 p-5 bg-white w-full tw-h-full shadow md:rounded-lg'>
+    <div className='flex justify-center md:mt-8'>
+      <form
+        className='md:w-3/4 m-0 p-5 bg-white w-full tw-h-full shadow md:rounded-lg rounded-lg'
+        onSubmit={onSubmit}>
         <div className='text-2xl text-indigo-900 text-center mb-4'>
           Create an exercise{' '}
         </div>
 
         <div className='flex-col flex py-3'>
           <label className='pb-2 text-gray-700 font-semibold'>User:</label>
-          <input
-            name='username'
-            onChange={onChange}
+
+          <select
             value={username}
-            type='text'
-            className='p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200'
-            placeholder='User Name'
-          />
+            onChange={onChange}
+            name='username'
+            className='flex w-4/12 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200 h-10 text-gray-600'>
+            {loaded ? (
+              users.map((item, idx) => (
+                <option key={idx} value={item}>
+                  {item}
+                </option>
+              ))
+            ) : (
+              <option> loading...</option>
+            )}
+          </select>
         </div>
 
         <div className='flex-col flex py-3'>
@@ -74,14 +99,15 @@ const CreateExercise = () => {
           </label>
           <div className='flex flex-row'>
             <select
+              required
               onChange={onChange}
               name='activity'
               className='flex w-4/12 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200 h-10 text-gray-600'
               placeholder=' or choose from list'
               id='grid-state'>
               {activity.sort().map((item, idx) => (
-                <option key={idx} value={item}>
-                  {item}
+                <option key={idx} value={item.toLowerCase()}>
+                  {item.toLowerCase()}
                 </option>
               ))}
             </select>
@@ -91,6 +117,7 @@ const CreateExercise = () => {
             </label>
 
             <input
+              required
               onChange={onChange}
               name='description'
               value={description}
@@ -104,9 +131,11 @@ const CreateExercise = () => {
         <div className='flex-col flex py-3'>
           <label className='pb-2 text-gray-700 font-semibold'>Duration:</label>
           <input
+            required
             onChange={onChange}
             name='duration'
             type='number'
+            minLength='0'
             value={duration}
             className='p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200 text-gray-600'
             placeholder='How many minutes'
@@ -116,6 +145,7 @@ const CreateExercise = () => {
         <div className='flex-col flex py-3'>
           <label className='pb-2 text-gray-700 font-semibold'>Date:</label>
           <DatePicker
+            required={true}
             selected={date}
             onChange={onDateChange}
             name='date'
@@ -127,7 +157,6 @@ const CreateExercise = () => {
 
         <div className='mt-2'>
           <button
-            onChange={onSubmit}
             className='rounded-lg p-3 bg-indigo-400 text-white w-full hover:bg-indigo-300'
             type='submit'>
             ADD EXERCISE

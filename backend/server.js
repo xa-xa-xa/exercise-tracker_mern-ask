2ba979ddb8cd3ca8768a882068be1f9878b6507a
xa-xa-xa <askrasn@gmail.com>
2020-01-19 15:26:50 -0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const publicPath = path.join(__dirname, '..', 'build');
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
 // DB endpoint
 const uri = process.env.DATABASE_URI;
 mongoose.connect(uri, {

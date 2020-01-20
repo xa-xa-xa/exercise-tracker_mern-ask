d3f367d631dd132fd9b6d1dd4e0af15eec109706
xa-xa-xa <askrasn@gmail.com>
2020-01-19 16:12:38 -0800

    app.use(express.static('build'));

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,9 @@ app.use('/users', usersRouter);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', '/build/index.html'));
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
   });
 }
 

f8ca608cb908d62ab8c0b0ea12d2ca559044e281
xa-xa-xa <askrasn@gmail.com>
2020-01-20 08:37:32 -0800

    res.sendFile(path.resolve(__dirname, '/build/index.html'));

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/public/index.html'));
+    res.sendFile(path.resolve(__dirname, '/build/index.html'));
   });
 }
 
